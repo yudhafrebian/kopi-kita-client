1f74d5202e8319eb4dfa2cc0c0a7c1180bd0371e
yudhafrebian <anandayudha777@gmail.com>
2025-09-04 15:49:28 +0700

    feat: refactor MenuPage to handle category search parameter correctly

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,15 +2,15 @@ import CTASection from "@/view/menu/CTA";
 import HeaderSection from "@/view/menu/Header";
 import MenuSection from "@/view/menu/Menu";
 
-export default function MenuPage({
-  searchParams,
-}: {
-  searchParams: { category?: string };
-}) {
+export default function MenuPage({ searchParams }: any) {
+  const category = Array.isArray(searchParams?.category)
+    ? searchParams?.category[0]
+    : searchParams?.category;
+
   return (
     <main>
       <HeaderSection />
-      <MenuSection searchParams={searchParams} />
+      <MenuSection searchParams={{ category }} />
       <CTASection />
     </main>
   );
