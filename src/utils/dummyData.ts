2c64368cf38f6b16651efa2cfd37b38dd760bb83
yudhafrebian <anandayudha777@gmail.com>
2025-09-15 15:14:30 +0700

    feat: update dummy data to use remote image URLs for featured products

--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -3,28 +3,28 @@ export const featuredProduct = [
         id: 1,
         name: "Espresso",
         description: "Kopi murni dengan rasa yang kuat dan aroma yang menggoda",
-        img_url: "/assets/espresso.jpg",
+        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923929/espresso_xjfawq.jpg",
         price: 18000
     },
     {
         id: 2,
         name: "Cappuccino",
         description: "Perpaduan sempurna espresso dengan susu berbusa halus",
-        img_url: "/assets/cappucino.jpg",
+        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923928/cappucino_m5jrm3.jpg",
         price: 28000
     },
     {
         id: 3,
         name: "Caramel Sea Salt Latte",
         description: "Latte creamy dengan caramel dan sentuhan garam laut",
-        img_url: "/assets/caramel.jpg",
+        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923929/caramel_femvpr.jpg",
         price: 32000
     },
     {
         id: 4,
         name: "Affogato",
         description: "Es krim vanilla yang disiram espresso panas",
-        img_url: "/assets/affogato.png",
+        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923928/affogato_khej4k.png",
         price: 30000
     }
 ]
