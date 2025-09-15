60fb391f802fd434d06c4c7d6a5b05beaf63290a
yudhafrebian <anandayudha777@gmail.com>
2025-09-15 19:38:33 +0700

    fix:typo on testimonies image cdn

--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -1,51 +1,61 @@
 export const featuredProduct = [
-    {
-        id: 1,
-        name: "Espresso",
-        description: "Kopi murni dengan rasa yang kuat dan aroma yang menggoda",
-        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923929/espresso_xjfawq.jpg",
-        price: 18000
-    },
-    {
-        id: 2,
-        name: "Cappuccino",
-        description: "Perpaduan sempurna espresso dengan susu berbusa halus",
-        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923928/cappucino_m5jrm3.jpg",
-        price: 28000
-    },
-    {
-        id: 3,
-        name: "Caramel Sea Salt Latte",
-        description: "Latte creamy dengan caramel dan sentuhan garam laut",
-        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923929/caramel_femvpr.jpg",
-        price: 32000
-    },
-    {
-        id: 4,
-        name: "Affogato",
-        description: "Es krim vanilla yang disiram espresso panas",
-        img_url: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923928/affogato_khej4k.png",
-        price: 30000
-    }
-]
+  {
+    id: 1,
+    name: "Espresso",
+    description: "Kopi murni dengan rasa yang kuat dan aroma yang menggoda",
+    img_url:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923929/espresso_xjfawq.jpg",
+    price: 18000,
+  },
+  {
+    id: 2,
+    name: "Cappuccino",
+    description: "Perpaduan sempurna espresso dengan susu berbusa halus",
+    img_url:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923928/cappucino_m5jrm3.jpg",
+    price: 28000,
+  },
+  {
+    id: 3,
+    name: "Caramel Sea Salt Latte",
+    description: "Latte creamy dengan caramel dan sentuhan garam laut",
+    img_url:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923929/caramel_femvpr.jpg",
+    price: 32000,
+  },
+  {
+    id: 4,
+    name: "Affogato",
+    description: "Es krim vanilla yang disiram espresso panas",
+    img_url:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto/v1757923928/affogato_khej4k.png",
+    price: 30000,
+  },
+];
 
 export const Testimonies = [
-    {
-        id:1,
-        name: "Sarah Dewi",
-        avatar: "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto,c_fill,48,h_48/v1757925052/sarah_e0bjmd.webp",
-        testimony: "Kopi terbaik di kota! Suasana yang nyaman dan pelayanan yang ramah. Cappuccino mereka benar-benar luar biasa.",
-    },
-    {
-        id:2,
-        name: "Budi Santoso",
-        avatar: "/https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto,c_fill,48,h_48/v1757925043/john_rtkunc.webp",
-        testimony: "Tempat favorit untuk meeting dan ngobrol santai. Kopinya selalu konsisten dan tempatnya instagramable banget!",
-    },
-    {
-        id:3,
-        name: "Maya Putri",
-        avatar: "https://res.cloudinary.com/dc4rzqyzq/image/f_auto,q_auto,c_fill,48,h_48/upload/v1757925041/maya_au7woe.webp",
-        testimony: "Sudah jadi langganan hampir 2 tahun. Kopi Kita tidak pernah mengecewakan. Highly recommended!",
-    }
-]
+  {
+    id: 1,
+    name: "Sarah Dewi",
+    avatar:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto,c_fill,w_48,h_48/v1757925052/sarah_e0bjmd.webp",
+    testimony:
+      "Kopi terbaik di kota! Suasana yang nyaman dan pelayanan yang ramah. Cappuccino mereka benar-benar luar biasa.",
+  },
+  {
+    id: 2,
+    name: "Budi Santoso",
+    avatar:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto,c_fill,w_48,h_48/v1757925043/john_rtkunc.webp",
+    testimony:
+      "Tempat favorit untuk meeting dan ngobrol santai. Kopinya selalu konsisten dan tempatnya instagramable banget!",
+  },
+  {
+    id: 3,
+    name: "Maya Putri",
+    avatar:
+      "https://res.cloudinary.com/dc4rzqyzq/image/upload/f_auto,q_auto,c_fill,w_48,h_48/v1757925041/maya_au7woe.webp",
+    testimony:
+      "Sudah jadi langganan hampir 2 tahun. Kopi Kita tidak pernah mengecewakan. Highly recommended!",
+  },
+];
